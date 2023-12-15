099dd3c7ec96887961c44896a6300995cbac193d
ochelle <alexizochelle@gmail.com>
2023-12-15 19:31:41 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+// index.js
+
+// Function expression for divide
+function divide(a, b) {
+    console.log('dividing ${divide}');
+    return 2000 / 100;
+}
+const square = x => x**2;
+
+const add = (a, b) => (a + b); 
